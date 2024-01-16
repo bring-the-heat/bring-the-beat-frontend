27ba931b496a8637d6f4b440d013a2bc360e3fad
atopchev <atopchev@berkeley.edu>
2024-01-15 22:59:14 -0500

    add gh-pages

--- src/components/SubmissionForm.jsx
+++ src/components/SubmissionForm.jsx
@@ -1,6 +1,89 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
+const SubmissionForm = () => {
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        songTitle: '',
+        songLink: '',
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const formElement = document.querySelector("form");
+        const formData = new FormData(formElement);
+        fetch(
+            "https://script.google.com/macros/s/AKfycbxfI4m5TQQuQkWH1afjG6AQvqFyum984JTkOMuY2pHalUS4gyYLOfO8lax6gb-KHXtS9Q/exec", 
+            { method: "POST", body: formData }
+        )
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+
+        console.log('Form data submitted:', formData);
+    };
+
+    return (
+        <FormContainer>
+            <h1>Enter your song for this week's round: </h1>
+            <Form className="form" onSubmit={handleSubmit}>
+                <Input
+                    type="text"
+                    name="firstName"
+                    placeholder="Your name 👀"
+                    value={formData.firstName}
+                    onChange={handleChange}
+                    required
+                />
+                <Input
+                    type="text"
+                    name="lastName"
+                    placeholder="Last name too pls"
+                    value={formData.lastName}
+                    onChange={handleChange}
+                    required
+                />
+                <Input
+                    type="text"
+                    name="songTitle"
+                    placeholder="Your song's title"
+                    value={formData.songTitle}
+                    onChange={handleChange}
+                    required
+                />
+                <Input
+                    type="text"
+                    name="songLink"
+                    placeholder="Spotify song link"
+                    value={formData.songLink}
+                    onChange={handleChange}
+                    required
+                />
+                <SubmitButton style={{marginTop: "100px"}} type="submit">SUBMIT</SubmitButton>
+                <hr style={{
+                    background: "#282c34",
+                    width: "150px",
+                    margin: "70px auto",
+                }} />
+            </Form>
+        </FormContainer>
+    );
+};
+
 const FormContainer = styled.div`
   max-width: 400px;
   margin: 0 auto;
@@ -33,74 +116,5 @@ const SubmitButton = styled.button`
     box-shadow: 0 0 30px rgba(255, 182, 15, 0.9);
   }
 `;
-
-const SubmissionForm = () => {
-    const [formData, setFormData] = useState({
-        firstName: '',
-        lastName: '',
-        songTitle: '',
-        songLink: '',
-    });
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData((prevData) => ({
-            ...prevData,
-            [name]: value,
-        }));
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        console.log('Form data submitted:', formData);
-    };
-
-    return (
-        <FormContainer>
-            <h1>Enter your song for this week's round: </h1>
-            <Form onSubmit={handleSubmit}>
-                <Input
-                    type="text"
-                    name="firstName"
-                    placeholder="Your name 👀"
-                    value={formData.firstName}
-                    onChange={handleChange}
-                    required
-                />
-                <Input
-                    type="text"
-                    name="lastName :* "
-                    placeholder="Last name too pls"
-                    value={formData.lastName}
-                    onChange={handleChange}
-                    required
-                />
-                <Input
-                    type="text"
-                    name="songTitle"
-                    placeholder="Your song's title"
-                    value={formData.songTitle}
-                    onChange={handleChange}
-                    required
-                />
-                <Input
-                    type="text"
-                    name="songLink"
-                    placeholder="Spotify song link"
-                    value={formData.songLink}
-                    onChange={handleChange}
-                    required
-                />
-                <SubmitButton style={{marginTop: "100px"}} type="submit">SUBMIT</SubmitButton>
-                <hr style={{
-                    background: "#282c34",
-                    width: "150px",
-                    margin: "70px auto",
-                }} />
-            </Form>
-        </FormContainer>
-    );
-};
 
 export default SubmissionForm;
